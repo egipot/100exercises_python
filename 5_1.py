e4cd0333d8f4609253d2e1e8de7cbd6a04f0e2e0
egipot <92053170+egipot@users.noreply.github.com>
2023-09-06 22:31:04 +0300

    Add files via upload

---
+++ 5_1.py
@@ -0,0 +1,10 @@
+#Complete the script so that it prints out the second letter of the list
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+print(letters[1])
+
+
+#egi solution: created "000_test.py"
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+print(letters[1])
+#the same solution as ardit's! yey!
+#ardit note: list indexing starts at 0
