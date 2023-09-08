bb34500860a3809643f2087ee391ba41cc1bf883
egipot <92053170+egipot@users.noreply.github.com>
2023-09-08 13:07:04 +0300

    Add files via upload

---
+++ 7_1.py
@@ -0,0 +1,17 @@
+#Complete the script so that it prints out a list containing the first three elements of list letters
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+
+#ardit solution:
+print(letters[:3])
+
+
+#egi solution: 
+def lowerLimit_func():
+    return 0
+
+upperLimit_var = 3
+print(letters[lowerLimit_func():upperLimit_var])
+
+#egi explanation: Learn other concepts and use flexible approach/solution to utilize flexibility
+
+#same output:  ['a', 'b', 'c']
