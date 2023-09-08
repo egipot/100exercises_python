3dc7c9f71c329db288e785a70e1c68d0e41eaf77
egipot <92053170+egipot@users.noreply.github.com>
2023-09-08 13:16:30 +0300

    Add files via upload

---
+++ 10_1.py
@@ -0,0 +1,9 @@
+#Question: Complete the script so that it prints out a list slice containing letters a, c, e, g, and i. 
+
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+
+#egi solution
+print(letters[::2])
+
+#ardit solution
+print(letters[::2]) #now you get it egipot!
