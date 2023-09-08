d749e38ed732ccac1395396c25ff2f9d5e644436
egipot <92053170+egipot@users.noreply.github.com>
2023-09-08 13:12:02 +0300

    Add files via upload

---
+++ 9_1.py
@@ -0,0 +1,9 @@
+#Question: 
+#Complete the script so that it prints out a list slice containing the last three items of the list letters .
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+
+#egi solution after many attempts :D
+print(letters[-3::1]) 
+
+#ardit solution: much simpler
+print(letters[-3:])
