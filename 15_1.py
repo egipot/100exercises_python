b5aeeb5a508bbb8b626bd0712d70e3f44c41d9ef
egipot <92053170+egipot@users.noreply.github.com>
2023-09-11 19:17:26 +0300

    Add files via upload

---
+++ 15_1.py
@@ -0,0 +1,11 @@
+#Question: Create a dictionary that contains the keys a  and b  and their respective values 1  and 2 .
+
+
+#egi solution
+new_dict = {'a': 1, 'b': 2} #dict uses curly brackets
+print(new_dict)
+print(type(new_dict))
+
+#hint with help
+new_dict2 = dict(a = 1, b = 2)
+print(new_dict2)
