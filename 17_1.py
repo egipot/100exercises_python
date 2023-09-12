79f48a0fd85b94d91340a7d018bd1e738badacce
egipot <92053170+egipot@users.noreply.github.com>
2023-09-12 21:24:09 +0300

    Add files via upload

---
+++ 17_1.py
@@ -0,0 +1,9 @@
+#Question: Calculate the sum of the values of keys a  and b .
+
+d = {"a": 1, "b": 2, "c": 3}
+
+#Expected output: 3  
+#Hint: Access the values as you did in the previous exercises.
+
+#egi solution:
+print(d["a"] +  d["b"])
