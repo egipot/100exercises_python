d32fd8830b607f64f6da63401aa532f4629bea6d
egipot <92053170+egipot@users.noreply.github.com>
2023-09-12 21:23:15 +0300

    Add files via upload

---
+++ 16_1.py
@@ -0,0 +1,9 @@
+#Question: Please complete the script so that it prints out the value of key b .
+
+d = {"a": 1, "b": 2}
+
+#Expected output: 2  
+#Hint: Dictionaries are exactly like lists. The difference is keys are the indexes. = square brackets
+
+#egi 
+print(d["b"])
