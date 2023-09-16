d78c71c9e023205154c91b79ea70f43d3e0b5e2f
egipot <92053170+egipot@users.noreply.github.com>
2023-09-16 21:37:39 +0300

    Add files via upload

---
+++ 32_.py
@@ -0,0 +1,11 @@
+# Question:  What will the following script output? Please try to do this by mind if you can.
+
+# egi got it right = 3 ;)
+
+c = 1
+def foo():
+    return c
+c = 3
+print(foo())
+
+# Hint: At the time when the function is called c  has a value of 3.
