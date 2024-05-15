360b90af5eb64ae790111f7f1bc2a52cbd681b2a
egipot <92053170+egipot@users.noreply.github.com>
2024-05-15 23:09:39 +0300

    Add files via upload

---
+++ 60_.py
@@ -0,0 +1,14 @@
+# Exercise 60: 
+# Create a program that prints Hello repeatedly.
+
+#Egi answer:
+while True:
+    print('Hello')
+#CTRL+C to interrupt
+
+
+#it turns out to be exactly the same as Ardit's answer, yay!
+#can also be:
+while 1 < 2:
+    print("Hello")
+    
